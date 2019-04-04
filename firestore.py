75f1f6052b2c0fb123cb93ed94b3a911233c7e38
Marius Kohmann <marius.kohmann@gmail.com>
2019-04-04 11:47:26 +0000

    fix

--- firestore.py
+++ firestore.py
@@ -2,24 +2,57 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+import nxppy
+import time
+
 #connecting to the database
 cred = credentials.Certificate('glive-29f4a-7c62255de9d1.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-#adding members in the database
-doc_ref = db.collection(u'users').document(u'aturing')
-doc_ref.set({
-    u'first': u'Alan',
-    u'middle': u'Mathison',
-    u'last': u'Turing',
-    u'born': 1912
-})
 
-#python prints the members
-users_ref = db.collection(u'users')
-docs = users_ref.get()
+def isAlowedToEnter(uid):
+	for doc in brukere:
+		if uid == doc:
+			a = brukere[uid]
+			if (a['isValid'] == True and a['isIntheGym'] == False):
+				print("user can enter")
+				a['isIntheGym'] = True
+				return True
+			elif a['isValid'] == False:
+				print("invalid card")
+			elif a['isIntheGym'] == True:
+				print ("user already in gym")
+			return False
+	return False
 
-for doc in docs:
-    print(u'{} => {}'.format(doc.id, doc.to_dict()))
+brukere ={}
+# Create a callback on_snapshot function to capture changes
+def on_snapshot(doc_snapshot, changes, read_time):
+	for doc in doc_snapshot:
+		brukere[doc.id] = doc.to_dict()
+
+doc_ref = db.collection(u'users')
+
+# Watch the document
+doc_watch = doc_ref.on_snapshot(on_snapshot)
+
+#set up scanner 
+mifare = nxppy.Mifare()
+print("Ready to scan")
+
+#wait for card to be scanned 
+while True:
+	try:
+		uid = mifare.select()
+		if uid:
+			print(uid)
+			if isAlowedToEnter(uid):
+				#user can enter 
+				doc_ref = db.collection(u'users').document(uid)
+				doc_ref.set({u'isIntheGym':True}, merge=True)
+	except nxppy.SelectError:
+        	pass
+	time.sleep(1)
+
