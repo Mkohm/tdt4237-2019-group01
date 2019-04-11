5a868fb3c3a044d27e5673ae631f57d5330b1bc7
helenlavr <helenlav@stud.ntnu.no>
2019-04-11 12:07:50 +0000

    in_scanner is now working, hurraay

--- inScanner.py
+++ inScanner.py
@@ -8,21 +8,13 @@ import time
 import pygame
 
 class inScanner:
-	
-	print("ja1")
 	brukere={}
-		
-	def __init__(self):
-		print("start")
-		self.mifare = nxppy.Mifare()
 	def isAlowedToEnter(self, uid):
 		for doc in self.brukere:
 			if uid == doc:
 				a = self.brukere[uid]
 				if (a['isValid'] == True and a['isIntheGym'] == False):
 					print("user can enter")
-					a['isIntheGym'] = True
-					
 					return True
 				elif a['isValid'] == False:
 					print("invalid card")
@@ -44,43 +36,47 @@ class inScanner:
 		pygame.mixer.music.play()
 		while pygame.mixer.music.get_busy() == True:
 			continue
-	
 	#set up scanner
 	print("Ready to scan")
 	
 	#wait for card to be scanned 
 	def start_scanner(self):
 		print("ready")
-		while True:
+		hasScanned = False
+		bb = nxppy.Mifare()
+		while not hasScanned:
 			try:	
 				
-				uid = nxppy.Mifare().select()
-				print(self.stm)
+				uid = bb.select()
 				if uid:
 					print("id: "+str(uid))
+					hasScanned = True
 					if self.isAlowedToEnter(uid):
 						print("valid")
 						self.stm.send('card_valid')
 						#send entry to firebase
-						#doc_ref = db.collection(u'users').document(uid)
-						#doc_ref.set({u'isIntheGym':True}, merge=True)
+						doc_ref = db.collection(u'users').document(uid)
+						doc_ref.set({u'isIntheGym':True, u'scanTime':firestore.SERVER_TIMESTAMP}, merge=True)
 					else:
 						print("invalid")
 						self.stm.send('card_invalid')
+					return
 			except nxppy.SelectError:
 				pass
 			time.sleep(1)
 	def approved(self):
 		print("lys grønnt")
-		playSound("sounds/enter.wav")
+		self.playSound("sounds/enter.wav")
 	def denied(self):
 		print("lys rødt")
-		playSound("sounds/accessDenied.mp3")
+		self.playSound("sounds/accessDenied.mp3")
 	def turn_off_leds(self):
 		print("klar")
 
 
+
 in_scanner = inScanner()
+
 #connecting to the database
 cred = credentials.Certificate('glive-29f4a-7c62255de9d1.json')
 firebase_admin.initialize_app(cred)
@@ -88,6 +84,9 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 # Create a callback on_snapshot function to capture changes
 def on_snapshot(doc_snapshot, changes, read_time):
+	for change in changes:
+		if change.type.name == 'MODIFIED':
+			print(change.document.id)
 	in_scanner.g(doc_snapshot)
 	
 doc_ref = db.collection(u'users')
@@ -97,10 +96,11 @@ doc_watch = doc_ref.on_snapshot(on_snapshot)
 
 #state machine
 
+
 #the transistions
 t0 = {'source':'initial',
 	'target':'idle',
-	'effect':'start_scanner; turn_off_leds'}
+	'effect':'start_scanner'}
 t1 = {'trigger':'card_valid',
 	'source':'idle',
 	'target':'green',
@@ -132,9 +132,7 @@ red = {'name':'red',
 machine = Machine(name='in_scanner',transitions=[t0, t1, t2, t3, t4, t5], obj = in_scanner,
 			states = [idle, green, red])
 in_scanner.stm = machine
-print(machine)
-print("starting")
+
 driver = Driver()
 driver.add_machine(machine)
 driver.start()
-
